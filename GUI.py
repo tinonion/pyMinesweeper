03bebec7a5cf9e2f7037d0f61af80df28f447cd4
tinonion <william.hunter.jones@gmail.com>
2018-03-24 12:40:21 -0400

    added support for jpg images and now generates core tiles for the game board

--- GUI.py
+++ GUI.py
@@ -1,20 +1,36 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
 
-class Grid(object):
+def genImage(path):
+    img = Image.open(path)
+    processed_img = ImageTk.PhotoImage(img)
+    return processed_img
+
+
+class GUI(object):
 
     def __init__(self, master):
         self.master = master
-
         self.tiles = []
+
+        image_dict = {
+            'unexp': genImage('./sprites/unexp.jpg'),
+            'exp_blank': genImage('./sprites/exp_blank.jpg')
+        }
+
+        def clicked(event):
+            tile.configure(image=image_dict['exp_blank'])
 
         for x in range(30):
             for y in range(16):
-                tile = Button(master, text='t')
-                self.tiles.append(tile)
+                tile = Label(master, image=image_dict['unexp'], borderwidth=0)
+                tile.image = image_dict['unexp']
                 tile.grid(row=y, column=x)
+                tile.bind('<Button-1>', clicked)
+                self.tiles.append(tile)
 
 
 root = Tk()
-gui = Grid(root)
+gui = GUI(root)
 root.mainloop()
