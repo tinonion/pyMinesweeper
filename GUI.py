0107952bd2107333f9624a35acb72a191e20e640
tinonion <william.hunter.jones@gmail.com>
2018-03-26 22:29:40 -0400

    now generates GUI with core tiles and border with minimal mouse control

--- GUI.py
+++ GUI.py
@@ -1,4 +1,4 @@
-from tkinter import *
+import tkinter as tk
 from PIL import Image, ImageTk
 
 
@@ -6,6 +6,60 @@ def genImage(path):
     img = Image.open(path)
     processed_img = ImageTk.PhotoImage(img)
     return processed_img
+
+
+def placeEle(master, img, x, y):
+    widget = tk.Label(master, image=img, borderwidth=0)
+    widget.image = img
+    widget.grid(column=x, row=y)
+    return widget
+
+
+class Input(object):
+
+    def __init__(self):
+        self.pressed = False
+
+
+class CoreTile(object):
+
+    def __init__(self, master, input_holder):
+        self.img_unexp = genImage('./sprites/unexp.jpg')
+        self.img_exp_blank = genImage('./sprites/exp_blank.jpg')
+        self.Label = tk.Label(master, borderwidth=0, image=self.img_unexp)
+        self.Label.image = self.img_unexp
+        self.Input = input_holder
+        self.mouse = None
+
+        def mouseEnter(event):
+            self.mouse = True
+            if self.Input.pressed:
+                self.Label.configure(image=self.img_exp_blank)
+
+        def mouseLeave(event):
+            self.mouse = False
+            print(self.Label.winfo_pointerxy())
+            if self.Input.pressed:
+                self.Label.configure(image=self.img_unexp)
+
+        def pressClick(event):
+            self.Input.pressed = True
+            self.Label.configure(image=self.img_exp_blank)
+
+        def releaseClick(event):
+            self.Input.pressed = False
+            if self.mouse:
+                self.Label.configure(image=self.img_exp_blank)
+            else:
+                self.Label.configure(image=self.img_unexp)
+
+        def pressedEnter(event):
+            self.Label.configure(image=self.img_exp_blank)
+
+        self.Label.bind('<Enter>', mouseEnter)
+        self.Label.bind('<Leave>', mouseLeave)
+        self.Label.bind('<ButtonPress-1>', pressClick)
+        self.Label.bind('<ButtonRelease-1>', releaseClick)
 
 
 class GUI(object):
@@ -16,21 +70,37 @@ class GUI(object):
 
         image_dict = {
             'unexp': genImage('./sprites/unexp.jpg'),
-            'exp_blank': genImage('./sprites/exp_blank.jpg')
+            'exp_blank': genImage('./sprites/exp_blank.jpg'),
+            'border_LR': genImage('./sprites/border_LR.jpg'),
+            'border_TB': genImage('./sprites/border_TB.jpg'),
+            'border_TLcorner': genImage('./sprites/border_TLcorner.jpg'),
+            'border_TRcorner': genImage('./sprites/border_TRcorner.jpg'),
+            'border_BLcorner': genImage('./sprites/border_BLcorner.jpg'),
+            'border_BRcorner': genImage('./sprites/border_BRcorner.jpg'),
         }
 
-        def clicked(event):
-            tile.configure(image=image_dict['exp_blank'])
+        for x in [0, 31]:
+            for y in range(1, 17):
+                placeEle(master, image_dict['border_LR'], x, y)
 
-        for x in range(30):
-            for y in range(16):
-                tile = Label(master, image=image_dict['unexp'], borderwidth=0)
-                tile.image = image_dict['unexp']
-                tile.grid(row=y, column=x)
-                tile.bind('<Button-1>', clicked)
+        for x in range(1, 31):
+            for y in [0, 17]:
+                placeEle(master, image_dict['border_TB'], x, y)
+
+        placeEle(master, image_dict['border_TRcorner'], 31, 0)
+        placeEle(master, image_dict['border_TLcorner'], 0, 0)
+        placeEle(master, image_dict['border_BRcorner'], 31, 17)
+        placeEle(master, image_dict['border_BLcorner'], 0, 17)
+
+        pressed = Input()
+
+        for x in range(1, 31):
+            for y in range(1, 17):
+                tile = CoreTile(master, pressed)
+                tile.Label.grid(column=x, row=y)
                 self.tiles.append(tile)
 
 
-root = Tk()
+root = tk.Tk()
 gui = GUI(root)
 root.mainloop()
